e8334e24ea92296909dbee41fedc61375463f278
MartimJales <mrtimjales@hotmail.com>
2021-12-04 11:38:59 +0000

    Auxiliar functions with Headers

--- auxlib/ft_split.c
+++ auxlib/ft_split.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 11:37:16 by mjales            #+#    #+#             */
+/*   Updated: 2021/12/04 11:37:17 by mjales           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //Allocates (with malloc(3)) and returns an array
 //of strings obtained by splitting ’s’ using the
 //character ’c’ as a delimiter. The array must be
--- auxlib/ft_striteri.c
+++ auxlib/ft_striteri.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 11:37:19 by mjales            #+#    #+#             */
+/*   Updated: 2021/12/04 11:37:20 by mjales           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //Applies the function f to each character of the
 //string passed as argument, and passing its index
 //as first argument. Each character is passed by
