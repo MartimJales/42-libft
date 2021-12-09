eca11999f5839cfee2cd95611c241c3b0a85289e
Jales <mrtimjales@hotmail.com>
2021-12-09 23:26:18 +0000

    Fix some notation errors

--- libft.h
+++ libft.h
@@ -42,7 +42,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 // Main functions
-int		ft_atoi(char *str);
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 int		ft_isalnum(char c);
@@ -64,8 +64,8 @@ int		ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_tolower(char *str);
-char	*ft_toupper(char *str);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 // Bonus functions
 void	ft_lstadd_back(t_list **lst, t_list *new);
