ac9d69f4f970ab37dbdfc134a26b6f31f7993381
Jales <mrtimjales@hotmail.com>
2021-12-17 11:41:03 +0000

    All 100%, for Tripouille tester

--- auxlib/ft_split.c
+++ auxlib/ft_split.c
@@ -12,8 +12,6 @@
 
 #include "libft.h"
 
-//I HAVE A BUG IN STRING_NUMBER, RETHINK IN THAT SHIT
-
 size_t	next_pos(char const *s, int c)
 {
 	size_t	i;
@@ -34,11 +32,9 @@ size_t	string_number(char const *s, int c)
 	flag = 0;
 	i = -1;
 	num = 0;
-	if (!c)
-		return (1);
 	while (s[++i])
 	{
-		if (s[i] == c)
+		if (s[i] != c)
 		{
 			if (!flag)
 				num++;
@@ -47,11 +43,6 @@ size_t	string_number(char const *s, int c)
 		else
 			flag = 0;
 	}
-	if (s[i] == c)
-		return (1);
-	if (s[0] == c || s[i - 1] == c)
-		num--;
-	//printf("str_num: %ld\n", num);
 	return (num);
 }
 
@@ -69,32 +60,15 @@ char	**ft_split(char const *s, char c)
 	num = string_number(s, c);
 	arr = malloc(sizeof(char *) * (num + 1));
 	i = -1;
-	//printf("num: %ld\n\n", num);
 	while (++i < num)
 	{
 		while (*s == c)
 			s++;
-		//printf("===(%ld)===\n", i);
-		//printf("loop: |%s|\n", s);
 		len = next_pos(s, c);
-		//printf("len: |%ld|\n", len);
 		arr[i] = malloc(len + 1);
 		ft_strlcpy(arr[i], s, len + 1);
 		s = s + len + 1;
 	}
-	arr[i] = malloc(1);
 	arr[i] = NULL;
 	return (arr);
 }
-
-//int	main(void)
-//{
-//	char	**tab;
-//	size_t	i;
-
-//	tab = ft_split("  tripouille  xx  ", ' ');
-//	i = -1;
-//	while (tab[++i])
-//		printf("stab[%ld]: |%s|\n", i, tab[i]);
-//	printf("tab[%ld]: |%s|\n", i, tab[i]);
-//}
